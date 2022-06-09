503fdbbfdc8910dd3c2a6a26b5ca0f9e1c1969bd
RTANC <34498692+RTANC@users.noreply.github.com>
2022-06-09 16:44:56 +0700

    update company select query

--- server/api/company/company.controller.js
+++ server/api/company/company.controller.js
@@ -3,7 +3,7 @@ const sequelize = require('../../connection')
 
 exports.getCompany = async (req, res, next) => {
     try {
-        const company = await sequelize.query("SELECT CompanyID, CompanyName FROM company where CompanyID > 0", { type: QueryTypes.SELECT });
+        const company = await sequelize.query("SELECT min(CompanyID) as CompanyID, min(StationID) as StationID, CompanyName FROM company where CompanyID > 0 and IsActive = 1 and CompanyType = 1 group by CompanyName order by CompanyName", { type: QueryTypes.SELECT });
         res.status(200).send(company)
     } catch (error) {
         next(error)

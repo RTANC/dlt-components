2f07d965cc179bf8fabc0bdc3323a78e84cd7e0a
RTANC <34498692+RTANC@users.noreply.github.com>
2022-10-27 21:12:42 +0700

    add gcs18

---
+++ report5/data/GCS01/GCS01-chart.js/content.js
@@ -0,0 +1,37 @@
+<script>
+var chart = new ApexCharts(document.querySelector("#myChart1"),{
+        title: {
+            text: 'จำนวนรถรับส่งสินค้า (คัน)',
+            align: 'center',
+            margin: 10
+        },
+        series: {{{toJSON ddata}}},
+        chart: {
+            width: 500,
+            type: 'pie',
+            animations: {
+                enabled: false
+            }
+        },
+        labels: ['ส่งสินค้า', 'รับสินค้า', 'รับและส่งสินค้า', 'อื่นๆ', 'รวม']
+    });
+chart.render();
+
+var chart2 = new ApexCharts(document.querySelector("#myChart2"),{
+        title: {
+            text: 'ปริมาณสินค้า (ตัน)',
+            align: 'center',
+            margin: 10
+        },
+        series: {{{toJSON ddata2}}},
+        chart: {
+            width: 500,
+            type: 'pie',
+            animations: {
+                enabled: false
+            }
+        },
+        labels: ['ส่งสินค้า', 'รับสินค้า', 'รับและส่งสินค้า', 'อื่นๆ', 'รวม']
+    });
+chart2.render(); 
+</script>

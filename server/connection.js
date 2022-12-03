408977fb4923e20f7d0e44853ad0514d62171b33
RTANC <34498692+RTANC@users.noreply.github.com>
2022-12-03 22:17:49 +0700

    fix time out

--- server/connection.js
+++ server/connection.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize('GCSDB', 'sa', 'rtanc1111100000', {
     host: 'localhost',
     dialect: 'mssql',
-    dialectOptions: {
-        requestTimeout: 300000
+    "dialectOptions": {
+        options: { "requestTimeout": 300000 }
     }
 })
 

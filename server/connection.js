3a202cbcbdd64341a4456c959392a84e54386821
RTANC <34498692+RTANC@users.noreply.github.com>
2023-02-02 09:19:08 +0700

    update connection.js

--- server/connection.js
+++ server/connection.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     dialect: 'mssql',
     port: process.env.DB_PORT,
     dialectOptions: {
-        options: { requestTimeout: 600000 }
+        options: { requestTimeout: 600000, encrypt: false }
     }
 })
 

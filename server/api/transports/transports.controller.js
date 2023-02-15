3c965adef8852d0550c908f5a9e7b9536ef5059b
RTANC <34498692+RTANC@users.noreply.github.com>
2023-02-15 11:28:45 +0700

    update date range getLicensePlates api

--- server/api/transports/transports.controller.js
+++ server/api/transports/transports.controller.js
@@ -1,6 +1,8 @@
 const { QueryTypes } = require('sequelize')
 const sequelize = require('../../connection')
-const { getDateTimeNow } = require('../../utils/utils')
+const { getDateTimeNow, dateTimeSQLFormatter } = require('../../utils/utils')
+const moment = require('moment')
+
 exports.getProvinces = async (req, res, next) => {
     try {
         const provinces = await sequelize.query("SELECT * FROM TxProvince where ProvinceID > 0 order by ProvinceName", { type: QueryTypes.SELECT })
@@ -14,7 +16,7 @@ exports.getLicensePlates = async (req, res, next) => {
     try {
         const sql = `select TOP (30) VehicleInID, StationID, TimeStampIn, LaneID, F1A, F1APID, (select ProvinceName from LPProvince where ProvinceID = F1APID) as F1APName, R1A, R1APID, (select ProvinceName from LPProvince where ProvinceID = R1APID) as R1APName, ImageRef, TransportID
         from VehicleIn
-        where TransportID IS NULL and VehicleOutID IS NULL and StationID = ${req.query.station} and (F1A like '${req.query.LPnumber}%' or R1A like '${req.query.LPnumber}%') and (TimeStampIn between '2021-06-01T00:00:00' and '2021-06-30T23:00:00')
+        where TransportID IS NULL and VehicleOutID IS NULL and StationID = ${req.query.station} and (F1A like '${req.query.LPnumber}%' or R1A like '${req.query.LPnumber}%') and (TimeStampIn between '${dateTimeSQLFormatter(moment().subtract(2, 'days').startOf('day'))}' and '${dateTimeSQLFormatter(moment())}')
         order by TimeStampIn desc` //ช่วงเวลา TimeStampIn ใช้สำหรับการ demo เท่านั้น
 
         const licenseplates = await sequelize.query(sql, { type: QueryTypes.SELECT })

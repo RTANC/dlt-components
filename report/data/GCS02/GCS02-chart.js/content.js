ebffb538374fd97af498f243cd13c44327b843fa
RTANC <34498692+RTANC@users.noreply.github.com>
2023-06-17 15:56:26 +0700

    reformatting GCS03

--- report/data/GCS02/GCS02-chart.js/content.js
+++ report/data/GCS02/GCS02-chart.js/content.js
@@ -254,112 +254,111 @@
     },
     series: {{{getDataChart3 reportData}}},
     chart: {
-    width: 1000,
-  height: 450,
-  type: 'bar',
-  animations: {enabled: false },
+      width: 1000,
+      height: 450,
+      type: 'bar',
+      animations: {enabled: false },
       fontFamily: 'THSarabunPSK'
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          position: 'top', // top, center, bottom
         },
-  plotOptions: {
-    bar: {
+      }
+    },
     dataLabels: {
-    position: 'top', // top, center, bottom
-            },
-          }
-        },
-  dataLabels: {
-    enabled: true,
-  formatter: function (val) {
-            return val.toLocaleString('en-US');
-          },
-  offsetY: -30,
-    style: {
-      fontSize: '16px',
-      fontFamily: 'THSarabunPSK',
-      fontWeight: 'bold',
-      colors: ["#304758"]
-    }
-        },
-
-  xaxis: {
-    labels: {
-    rotate: -45,
-    style: {
-        fontSize:  '18px',
-        fontWeight:  'bold',
-        fontFamily:  'THSarabunPSK',
-        color:  "#263238"
+      enabled: true,
+      formatter: function (val) {
+        return val.toLocaleString('en-US');
+      },
+      offsetY: -30,
+      style: {
+        fontSize: '16px',
+        fontFamily: 'THSarabunPSK',
+        fontWeight: 'bold',
+        colors: ["#304758"]
       }
-          },
-  categories: {{{getCategoriesChart reportData}}},
-  axisBorder: {
-    show: false
-          },
-  axisTicks: {
-    show: false
-          },
-  crosshairs: {
-    fill: {
-    type: 'gradient',
-  gradient: {
-    colorFrom: '#D8E3F0',
-  colorTo: '#BED1E6',
-  stops: [0, 100],
-  opacityFrom: 0.4,
-  opacityTo: 0.5,
-              }
-            }
-          },
-  tooltip: {
-    enabled: true,
-          },
-  title: {
-    text: 'ประเภทรถ',
-    offsetY: 220
-          }
-        },
-  yaxis: {
-    axisBorder: {
-    show: true
-          },
-  axisTicks: {
-    show: true,
-          },
-  labels: {
-    show: true,
-  formatter: function (val) {
-              return val.toLocaleString('en-US');
-            },
-    style: {
-        fontSize:  '18px',
-        fontWeight:  'bold',
-        fontFamily:  'THSarabunPSK',
-        color:  "#263238"
-      }
-          },
-  title: {
-    text: 'ปริมาณสินค้า (ตัน)',
-    style: {
-        fontSize:  '24px',
-        fontWeight:  'bold',
-        fontFamily:  'THSarabunPSK',
-        color:  "#263238"
-      }
-          }        
-        },
-  title: {
-    text: 'ปริมาณสินค้ารับส่ง (ตัน)',
-  floating: false,
-  offsetY: 0,
-  align: 'center',
-  style: {
-        fontSize:  '24px',
-        fontWeight:  'bold',
-        fontFamily:  'THSarabunPSK',
-        color:  "#263238"
-      }
+    },
+    xaxis: {
+      labels: {
+        rotate: -45,
+        style: {
+          fontSize:  '18px',
+          fontWeight:  'bold',
+          fontFamily:  'THSarabunPSK',
+          color:  "#263238"
         }
-        });
+      },
+      categories: {{{getCategoriesChart reportData}}},
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false
+      },
+      crosshairs: {
+        fill: {
+          type: 'gradient',
+          gradient: {
+            colorFrom: '#D8E3F0',
+            colorTo: '#BED1E6',
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
+          }
+        }
+      },
+      tooltip: {
+        enabled: true,
+      },
+      title: {
+        text: 'ประเภทรถ',
+        offsetY: 220
+      }
+    },
+    yaxis: {
+      axisBorder: {
+        show: true
+      },
+      axisTicks: {
+        show: true,
+      },
+      labels: {
+        show: true,
+        formatter: function (val) {
+          return val.toLocaleString('en-US');
+        },
+        style: {
+          fontSize:  '18px',
+          fontWeight:  'bold',
+          fontFamily:  'THSarabunPSK',
+          color:  "#263238"
+        }
+      },
+      title: {
+        text: 'ปริมาณสินค้า (ตัน)',
+        style: {
+          fontSize:  '24px',
+          fontWeight:  'bold',
+          fontFamily:  'THSarabunPSK',
+          color:  "#263238"
+        }
+      }
+    },
+    title: {
+      text: 'ปริมาณสินค้ารับส่ง (ตัน)',
+      floating: false,
+      offsetY: 0,
+      align: 'center',
+      style: {
+        fontSize:  '24px',
+        fontWeight:  'bold',
+        fontFamily:  'THSarabunPSK',
+        color:  "#263238"
+      }
+    }
+  });
   chart3.render();
 
 </script>
--- report/data/GCS03/GCS03-chart.js/content.js
+++ report/data/GCS03/GCS03-chart.js/content.js
@@ -1,242 +1,225 @@
 <script>
-var chart1 = new ApexCharts(document.querySelector("#chart1"), {
+  const legendStyle = {
+    fontSize: '26px',
+    fontFamily: 'THSarabunPSK',
+    fontWeight:  'bold'
+  }
+
+  const dataLabelStyle = {
+    fontSize: '16px',
+    fontWeight: 'bold',
+    colors: ["#304758"]
+  }
+
+  const xyLabelStyle = {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    colors: '#263238'
+  }
+
+  const xyTitleStyle = {
+    fontSize: '20px',
+    fontFamily: 'THSarabunPSK',
+    fontWeight: 'bold',
+    colors: '#263238'
+  }
+
+  const titleStyle = {
+    fontSize: '20px',
+    fontFamily: 'THSarabunPSK',
+    fontWeight: 'bold',
+    colors: '#263238'
+  }
+
+  var chart1 = new ApexCharts(document.querySelector("#chart1"), {
     legend: {
       position: 'top',
       horizontalAlign: 'right',
       labels: {
         useSeriesColors: true
       },
-      fontSize: '26px',
-      fontFamily: 'THSarabunPSK',
-      fontWeight:  'bold'
+      fontSize: legendStyle.fontSize,
+      fontFamily: legendStyle.fontFamily,
+      fontWeight:  legendStyle.fontWeight
     },
     series: {{{getDataChart1 reportData}}},
     chart: {
-    width: 1000,
-  height: 450,
-  type: 'bar',
-  animations: {enabled: false },
-  fontFamily: 'THSarabunPSK'
-        },
-  plotOptions: {
-    bar: {
+      width: 1000,
+      height: 450,
+      type: 'bar',
+      animations: {enabled: false },
+      fontFamily: 'THSarabunPSK'
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          position: 'top', // top, center, bottom
+        }
+      }
+    },
     dataLabels: {
-    position: 'top', // top, center, bottom
-            },
+      enabled: true,
+      formatter: function (val) {
+        return val.toLocaleString('en-US');
+      },
+      offsetY: -30,
+      style: dataLabelStyle
+    },
+    xaxis: {
+      labels: {
+        rotate: -45,
+        style: xyLabelStyle
+      },
+      categories: {{{getCategories reportData}}},
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false
+      },
+      crosshairs: {
+        fill: {
+          type: 'gradient',
+          gradient: {
+            colorFrom: '#D8E3F0',
+            colorTo: '#BED1E6',
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
           }
-        },
-  dataLabels: {
-    enabled: true,
-  formatter: function (val) {
-            return val.toLocaleString('en-US');
-          },
-  offsetY: -30,
-  style: {
-        fontSize: '16px',
-        fontFamily: 'THSarabunPSK',
-        fontWeight: 'bold',
-        colors: ["#304758"]
-      }
-        },
-
-  xaxis: {
-    labels: {
-    rotate: -45,
-    style: {
-        fontSize: '18px',
-        fontFamily: 'THSarabunPSK',
-        fontWeight: 'bold',
-        colors: '#263238'
-      }
-          },
-  categories: {{{getCategories reportData}}},
-  axisBorder: {
-    show: false
-          },
-  axisTicks: {
-    show: false
-          },
-  crosshairs: {
-    fill: {
-    type: 'gradient',
-  gradient: {
-    colorFrom: '#D8E3F0',
-  colorTo: '#BED1E6',
-  stops: [0, 100],
-  opacityFrom: 0.4,
-  opacityTo: 0.5,
-              }
-            }
-          },
-  tooltip: {
-    enabled: true,
-          },
-  title: {
-    text: 'ผู้ประกอบการ',
-    offsetY: 200,
-    style: {
-        fontSize: '20px',
-        fontFamily: 'THSarabunPSK',
-        fontWeight: 'bold',
-        colors: '#263238'
-      }
-          }
-        },
-  yaxis: {
-    axisBorder: {
-    show: true
-          },
-  axisTicks: {
-    show: true,
-          },
-  labels: {
-    show: true,
-  formatter: function (val) {
-              return val.toLocaleString('en-US');
-            },
-          style: {
-          fontSize:  '18px',
-          fontWeight:  'bold',
-          fontFamily:  'THSarabunPSK',
-          color:  '#263238'
         }
-          },
-  title: {
-    text: 'จำนวนรถ (คัน)',
-    style: {
-        fontSize: '20px',
-        fontFamily: 'THSarabunPSK',
-        fontWeight: 'bold',
-        colors: '#263238'
+      },
+      tooltip: {
+        enabled: true,
+      },
+      title: {
+        text: 'ผู้ประกอบการ',
+        offsetY: 200,
+        style: xyTitleStyle
       }
-          }        
+    },
+    yaxis: {
+      axisBorder: {
+        show: true
+      },
+      axisTicks: {
+        show: true,
+      },
+      labels: {
+        show: true,
+        formatter: function (val) {
+          return val.toLocaleString('en-US');
         },
-  title: {
-    text: 'จำนวนรถรับส่งสินค้า (คัน)',
-  floating: false,
-  offsetY: 0,
-  align: 'center',
-  style: {
-        fontSize: '20px',
-        fontFamily: 'THSarabunPSK',
-        fontWeight: 'bold',
-        colors: '#263238'
-      }
-        }
-        });
+        style: xyLabelStyle
+      },
+      title: {
+        text: 'จำนวนรถ (คัน)',
+        style: xyTitleStyle
+      }        
+    },
+    title: {
+      text: 'จำนวนรถรับส่งสินค้า (คัน)',
+      floating: false,
+      offsetY: 0,
+      align: 'center',
+      style: titleStyle
+    }
+  });
   chart1.render();
 
-    var chart2 = new ApexCharts(document.querySelector("#chart2"), {
+  var chart2 = new ApexCharts(document.querySelector("#chart2"), {
     legend: {
       position: 'top',
       horizontalAlign: 'right',
       labels: {
         useSeriesColors: true
       },
-      fontSize: '26px',
-      fontFamily: 'THSarabunPSK',
-      fontWeight:  'bold'
+      fontSize: legendStyle.fontSize,
+      fontFamily: legendStyle.fontFamily,
+      fontWeight:  legendStyle.fontWeight
     },
     series: {{{getDataChart2 reportData}}},
     chart: {
-    width: 1000,
-  height: 450,
-  type: 'bar',
-  animations: {enabled: false },
-  fontFamily: 'THSarabunPSK'
+      width: 1000,
+      height: 450,
+      type: 'bar',
+      animations: {enabled: false },
+      fontFamily: 'THSarabunPSK'
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          position: 'top', // top, center, bottom
         },
-  plotOptions: {
-    bar: {
+      }
+    },
     dataLabels: {
-    position: 'top', // top, center, bottom
-            },
+      enabled: true,
+      formatter: function (val) {
+        return val.toLocaleString('en-US');
+      },
+      offsetY: -30,
+      style: dataLabelStyle
+    },
+    xaxis: {
+      labels: {
+        rotate: -45,
+        style: xyLabelStyle
+      },
+      categories: {{{getCategories reportData}}},
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false
+      },
+      crosshairs: {
+        fill: {
+          type: 'gradient',
+          gradient: {
+            colorFrom: '#D8E3F0',
+            colorTo: '#BED1E6',
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
           }
-        },
-  dataLabels: {
-    enabled: true,
-  formatter: function (val) {
-            return val.toLocaleString('en-US');
-          },
-  offsetY: -30,
-  style: {
-    fontSize: '16px',
-  colors: ["#304758"]
-          }
-        },
-  xaxis: {
-    labels: {
-    rotate: -45,
-    style: {
-      fontSize: '16px',
-  colors: '#263238'
-    }
-          },
-  categories: {{{getCategories reportData}}},
-  axisBorder: {
-    show: false
-          },
-  axisTicks: {
-    show: false
-          },
-  crosshairs: {
-    fill: {
-    type: 'gradient',
-  gradient: {
-    colorFrom: '#D8E3F0',
-  colorTo: '#BED1E6',
-  stops: [0, 100],
-  opacityFrom: 0.4,
-  opacityTo: 0.5,
-              }
-            }
-          },
-  tooltip: {
-    enabled: true,
-          },
-  title: {
-    text: 'ผู้ประกอบการ',
-    offsetY: 220,
-    style: {
-      fontSize: '18px',
-  colors: ["#304758"]
-    }
-          }
-        },
-  yaxis: {
-    axisBorder: {
-    show: true
-          },
-  axisTicks: {
-    show: true,
-          },
-  labels: {
-    show: true,
-  formatter: function (val) {
-              return val.toLocaleString('en-US');
-            },
-            style: {
-      fontSize: '16px',
-  colors: ["#304758"]
-    }
-          },
-  title: {
-    text: 'ปริมาณสินค้า (ตัน)',
-    style: {
-      fontSize: '18px',
-  colors: ["#304758"]
-    }
-          }        
-        },
-  title: {
-    text: 'ปริมาณสินค้ารับส่ง (ตัน)',
-  floating: false,
-  offsetY: 0,
-  align: 'center',
-  style: {
-      fontSize: '18px',
-  colors: ["#304758"]
-    }
         }
-        });
+      },
+      tooltip: {
+        enabled: true,
+      },
+      title: {
+        text: 'ผู้ประกอบการ',
+        offsetY: 220,
+        style: xyTitleStyle
+      }
+    },
+    yaxis: {
+      axisBorder: {
+        show: true
+      },
+      axisTicks: {
+        show: true,
+      },
+      labels: {
+        show: true,
+        formatter: function (val) {
+          return val.toLocaleString('en-US');
+        },
+        style: xyLabelStyle
+      },
+      title: {
+        text: 'ปริมาณสินค้า (ตัน)',
+        style: xyTitleStyle
+      }        
+    },
+    title: {
+      text: 'ปริมาณสินค้ารับส่ง (ตัน)',
+      floating: false,
+      offsetY: 0,
+      align: 'center',
+      style: titleStyle
+    }
+  });
   chart2.render();
 </script>

b125780efe0a43ab66694c736d0111b01b55ab78
RTANC <34498692+RTANC@users.noreply.github.com>
2024-12-26 09:34:35 +0700

    update index

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const { getStatics } = require('./statics.controller')
-// const { getVehicleIn, getVehicleOut } = require('./vehicles.controller')
+const { getVehicleIn, getVehicleOut } = require('./vehicles.controller')
 
 router.post('/static', getStatics)
-// router.post('/vehiclein', getVehicleIn)
-// router.post('/vehicleout', getVehicleOut)
+router.post('/vehiclein', getVehicleIn)
+router.post('/vehicleout', getVehicleOut)
 
 module.exports = router

2e7f96fffd0e9b8496e3e744a4caf28bf6995d07
RTANC <34498692+RTANC@users.noreply.github.com>
2024-12-27 08:45:40 +0700

    fix statics api

--- server/api/v1/statics.controller.js
+++ server/api/v1/statics.controller.js
@@ -16,7 +16,7 @@ exports.getStatics = async (req, res, next) => {
             where StationID = ${req.body.StationID} and LaneID = ${req.body.LaneID}
             order by TimeStamp DESC`, { type: QueryTypes.SELECT });
         } else {
-            resp.ResultData = await sequelize.query(`select TOP 1 RunID, StationID, TimeStamp, SeqNumber, LaneID, (select Description from VehicleClass where VehicleClass.VehicleClassID = static.VehicleClassID) [VehicleClass], LPR_Number, (select ProvinceName from LPProvince where ProvinceID = LPR_ProvinceID) [LPRProv], LPR_ImageName, VehicleImageName, Goods, DepartedFrom, Destination, Notes, GVW_Weight_max, GVW_Weight_Measured, GVW_Weight_Over, IsOverWeight
+            resp.ResultData = await sequelize.query(`select RunID, StationID, TimeStamp, SeqNumber, LaneID, (select Description from VehicleClass where VehicleClass.VehicleClassID = static.VehicleClassID) [VehicleClass], LPR_Number, (select ProvinceName from LPProvince where ProvinceID = LPR_ProvinceID) [LPRProv], LPR_ImageName, VehicleImageName, Goods, DepartedFrom, Destination, Notes, GVW_Weight_max, GVW_Weight_Measured, GVW_Weight_Over, IsOverWeight
             from static
             where StationID = ${req.body.StationID} and TimeStamp BETWEEN '${moment(req.body.DateWt).startOf('day').format('YYYY-MM-DDTHH:mm:ss')}' AND '${moment(req.body.DateWt).endOf('day').format('YYYY-MM-DDTHH:mm:ss')}' and LaneID = ${req.body.LaneID}
             order by TimeStamp DESC`, { type: QueryTypes.SELECT });
